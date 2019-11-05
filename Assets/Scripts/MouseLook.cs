ef91de28cd32a289398a4c685ac02f3c80bd2215
edusrmt <cealvesarmento@gmail.com>
2019-11-04 23:49:37 -0300

    Movimentação do jogador implementada

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+	[SerializeField]
+    float lookSensitivity = 2f;
+    [SerializeField]
+	float lookSmoothDamp = 0.1f;
+
+	float xRotation;
+	float yRotation;
+	float currentXRotation;
+	[HideInInspector]
+    public float currentYRotation;
+	float xRotationV;
+	float yRotationV;
+
+    void Update () {
+        xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+        yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothDamp);
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothDamp);
+
+        transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 6f;
+    [SerializeField]
+    bool limitDiagonalSpeed = true;
+    [SerializeField]
+    float antiBumpFactor = .75f;
+
+    MouseLook cameraLook;
+    Vector3 moveDirection = Vector3.zero;
+	CharacterController controller;
+	float slideLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        cameraLook = GetComponentInChildren<MouseLook>();
+        slideLimit = controller.slopeLimit - .1f;
+    }
+
+    // FixedUpdate is called once per fixed frame-rate frame
+    void FixedUpdate()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // If both horizontal and vertical are used simultaneously, limit speed (if allowed), so the total doesn't exceed normal move speed.
+        float inputModifyFactor = (inputX != 0f && inputY != 0f && limitDiagonalSpeed) ? .7071f : 1f;
+
+        // Rotates the player based on the camera rotation
+        transform.rotation = Quaternion.Euler (0, cameraLook.currentYRotation, 0);
+
+        // Recalculate moveDirection directly from axes, adding a bit of -y to avoid bumping down inclines
+        moveDirection = new Vector3(inputX * inputModifyFactor, -antiBumpFactor, inputY * inputModifyFactor);
+        moveDirection = transform.TransformDirection(moveDirection) * speed;
+        
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
